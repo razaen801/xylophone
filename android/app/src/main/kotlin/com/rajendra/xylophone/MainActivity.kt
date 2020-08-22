258312a9720c2a693e7c83b39b9ffe5f713fd74f
Rajendra <ras9843164801@gmail.com>
2020-08-22 04:20:49 -0400

    Initial push

---
+++ android/app/src/main/kotlin/com/rajendra/xylophone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rajendra.xylophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(XylophoneAapp());
+}
+
+class XylophoneAapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
